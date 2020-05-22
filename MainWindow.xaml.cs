813803c98e0b7f6992d157714429200f643c6c97
lzukw <zukw@litec.ac.at>
2020-05-22 11:04:45 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace WirelessDisplayServerGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected string _pathToExecutable { get; }
+
+        protected string _nameOfExecutable { get;  }
+
+        protected Process _backgroundProcess { get; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //
+            // Find out local IP-Address and put it into labelIp
+            //
+            string ipv4Address = "unknown";
+            string sHostName = System.Net.Dns.GetHostName();
+            System.Net.IPHostEntry ipE = System.Net.Dns.GetHostEntry(sHostName); 
+            foreach (System.Net.IPAddress ip in ipE.AddressList)
+            {
+                if ( ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork ) //ipv4
+                {
+                        ipv4Address = ip.ToString();
+                }
+            }
+
+            labelIp.Content = ipv4Address;
+
+            //
+            // Read in Configuration and spawn process
+            //
+            _pathToExecutable = ConfigurationManager.AppSettings["PathToWirelessDisplayServerExe"];
+            FileInfo executable = new FileInfo(_pathToExecutable);
+            if ( ! executable.Exists )
+            {
+                throw new FileNotFoundException($"Path to WirelessDisplayServer-Executable doesn't exist: '{executable.FullName}'");
+            }
+
+            // first kill old processes
+            string processName = System.IO.Path.GetFileNameWithoutExtension(executable.FullName);
+            Process[] ps = Process.GetProcessesByName(processName);
+            foreach (Process p in ps)
+            {
+                p.Kill();
+            }
+
+            // spawn process
+            _backgroundProcess = new Process();
+            _backgroundProcess.StartInfo.CreateNoWindow = true;
+            _backgroundProcess.StartInfo.UseShellExecute = false;
+            _backgroundProcess.StartInfo.RedirectStandardInput = true;
+            _backgroundProcess.StartInfo.RedirectStandardOutput = true;
+            _backgroundProcess.StartInfo.RedirectStandardError = true;
+            _backgroundProcess.StartInfo.WorkingDirectory = executable.DirectoryName;
+            _backgroundProcess.StartInfo.FileName = executable.FullName;
+            _backgroundProcess.OutputDataReceived += backgroundProcess_DataReceived;
+            _backgroundProcess.ErrorDataReceived += backgroundProcess_DataReceived;
+
+            bool processStarted = _backgroundProcess.Start();
+            if (! processStarted)
+            {
+                throw new Exception("Could not start WirelessDisplayServer as background-process!");
+            }
+            // This is necessary to start a thread, that receives the things written to
+            // stdout and stderr by the process. 
+            _backgroundProcess.BeginOutputReadLine();
+            _backgroundProcess.BeginErrorReadLine();
+
+       }
+
+        void backgroundProcess_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // This event-Handler runs in another thread (not the GUI-Thread), so it
+            // needs to use Dispatcher.Invoke in order to change GUI-Elements.
+            if ( ! String.IsNullOrEmpty(e.Data) )
+            {
+                Dispatcher.Invoke( ()=>{textblockLog.Text += e.Data + "\n"; }  );
+            }
+        }
+    
+        void mainWindow_Closing(object sender, object e)
+        {
+            // Stop background-process (WirelessDisplayServer)
+            _backgroundProcess.Kill();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,94 @@
+# WirelessDisplayServerGUI
+
+The purpose of this program is to run the program 'WirelessDisplayServer'
+in background. A GUI is displayed, that shows
+
+- the IP-Adress of this Computer running WirelessDispalyServer, and
+- the log-ouput of the WirelessDisplayServer
+
+## Configuration
+
+During development configuration is stored in the file App.config. After
+publishing the program with `dotnet publish` this configuration is stored
+in the file `WirelessDisplayServerGUI.dll.config`, and can be edited there.
+
+The necessary confifuration consists only of: 
+
+- A relative path to the the executable version of WirelessDisplayServer
+
+## Running the program
+
+Just use `dotnet run`.
+
+## Creating an executable
+
+From within the folder `WirelessDisplayServerGUI` run the following commands:
+
+```
+mkdir ..\WirelessDisplayServerGUI_executable
+dotnet publish -c Release -o ..\WirelessDisplayServerGUI_executable -r win-x64 --self-contained
+```
+The paremter `--self-contained` creates a 'stand-alone' executable version. This 
+paremeter can be omitted, if .NET-Core version 3.1 is installed on the target system.
+All necessary files are put in the directory `WirelessDisplayServerGUI_executable`.
+The executable to start is called `WirelessDisplayServerGUI.exe`. The configuration
+can still be changed, by changing the contents of `WirelessDisplayServerGUI.dll.config`
+(which is just the original `App.config`-file copied and renamed).
+
+## Technical Details
+
+Program created with:
+```
+mkdir WirelessDisplayServerGUI
+cd WirelessDisplayServerGUI
+dotnet new wpf
+```
+
+This creates the following files:
+
+- `App.xaml` and `App.xaml.cs`: These files together define the class `App`.
+- `MainWindow.xaml` and `MainWindow.xaml.cs`: These files define the class `MainWindow`.
+- `WirelessDisplayServerGUI.csproj` and `AssemblyInfo.cs`.
+
+From these files only `MainWindow.xaml` and `MainWindow.xaml.cs` were modified.
+
+MainWindow.xaml contains two relecant UI-Elemnts:
+
+- `labelIp` ...to display the local IP-Address.
+- `textblockLog` ...to display the log-output or WirelessDisplayServer.
+
+### Configuration
+
+- Added file `App.config`, containing the key-value-pair `PathToWirelessDisplayServerExe`.
+- Added `using System.Configuration;` to `MainWindow.xaml.cs`.
+- Configuration parameters are read with 
+  `ConfigurationManager.AppSettings["PathToWirelessDisplayServerExe"]`
+
+### MainWindow.xaml.cs
+
+This file contains the 'Interaction logic for `MainWindow.xaml`' by means of a
+partial class `MainWindow`. (The other parts of this class are created by the
+compiler from the file `MainWindow.xaml` and are only present in the 
+`obj`-directory).
+
+In `MainWindow.xaml.cs` the whole program is realized. In the Constructor of
+the `MainWindow`-class the following things are done:
+
+- The local IPv4-Address is retrieved by using static methods from 
+  `System.Net.Dns`. The IPv4-Address then is displayed to the user (inserted
+  into labelIp).
+- Configuration is read in from `App.config` containing the filepath to the
+  WirelessDisplayServer-executable. 
+- Eventually still running WirelessDisplayServer-processes are killed.
+- A new WirelessDisplayServer-process is created and run in background.
+- Two event-handlers are registered: Each time the started 
+  WirelessDisplayServer-background-process writes a line to its stdout or stderr,
+  the events `OutputDataReceived` or `ErrorDataReceived` occur. Both events call
+  the method `void backgroundProcess_DataReceived(object sender, DataReceivedEventArgs e)`,
+  where the line written by the process to stdout/stderr is contained in `e.Data`.
+  In this method, the line is appended to `textblockLog` and so displayed to
+  the user.
+
+When the user closes the MainWindow (and the whole program), the event-handler
+`void mainWindow_Closing(object sender, object e)` is called. Here the
+WirelessDisplayServer-background-process is killed.
